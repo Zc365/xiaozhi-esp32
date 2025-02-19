bc3ec771c7f3b416f28ed0e3bde60ed91397f627
zczc365 <zczc365@gmail.com>
2025-02-19 21:13:56 +0800

    update location

--- scripts/复制固件.bat
+++ scripts/复制固件.bat
@@ -1,4 +1,4 @@
 
-copy /Y ..\build\xiaozhi.bin ..\..\..\..\Desktop\ESP\Project\XiaozhiAI\V1.1.2\yunliao_C3.bin
+copy /Y ..\build\xiaozhi.bin ..\..\..\..\Desktop\ESP\Project\XiaozhiAI\release\v1.1.2_xiaozhiyunliao\yunliao_C3.bin
 
 ping -n 5 localhost >nul
